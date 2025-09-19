dd0884d69c3608c4eacf691af33410a0f88361d3
Gustavo Santos <santosgustavo52579@gmail.com>
2025-09-19 20:40:15 -0300

    Herança

--- 09_Heranca/Pessoa.cs
+++ 09_Heranca/Pessoa.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _09_Heranca
 {
+
+   
     internal abstract class Pessoa
     {
+        public int Id {  get; set; }
         public string Nome;
         public string Telefone;
 
@@ -17,5 +21,26 @@ namespace _09_Heranca
         {
             Console.WriteLine($"LISTA DE PESSOAS\n\nNome: {this.Nome} Documento: {this.GetDocumento()} Telefone: {this.Telefone}");
         }
+        public override string ToString() 
+        {
+            return $"<Tostring()> Nome: {{this.Nome}} Documento: {{this.GetDocumento()}} Telefone: {{this.Telefone}}\"";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+
+            //cast=> COnversão de objeto
+            //(pessoa)obj)=> Converte obj que é tipo object para tipo Pessoa
+            return this.Id == ((Pessoa)obj).Id; 
+        }
+
+        public override int GetHashCode()
+        {
+            return this. Id.GetHashCode();
+        }
     }
+    
 }
